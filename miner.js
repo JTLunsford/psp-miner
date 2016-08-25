85ad46c435d420e72ca9d33ffac36831060cac4f
Joshua Lunsford <josh_lunsford@homedepot.com>
2016-08-25 17:37:38 +0000

    adding handler

--- miner.js
+++ miner.js
@@ -2,20 +2,33 @@
 
 const path = require('path');
 const spawn = require('child_process').spawn;
+const exec = require('child_process').exec;
 
 const cli = require('cli');
 const _ = require('lodash');
 const _async = require('async');
 const jsome = require('jsome');
+const eventHandler = require('./event-handler');
 
 exports.opts = {
-	start: 	['s', 'start the capture'],
-	fd:	['f', 'capture file descriptors'],
-	proc:   ['p', 'capture the child/parent relationships/']
+	start: 		['s', 'start the capture'],
+	fd:		['f', 'capture file descriptors'],
+	proc:   	['p', 'capture the child/parent relationships'],
+	testchild:	['t', 'add a test child/parent']
 };
 
+
+let event;
 exports.load = (args, opts, cb) => {
 	if(opts.start){
+		event = eventHandler(opts.url);	
+		
+		if(opts.testchild) {
+			setTimeout(() => {
+				exec('(sleep 5; echo "test";)');
+			},10000);
+		}
+
 		let sysdig = spawn('sysdig',['evt.type!=switch', 'and', 'proc.name!=V8', 'and', 'proc.name!=node', 'and', 'proc.name!=sshd', 'and', 'proc.name!=sysdig']);
 		sysdig.stdout.setEncoding('utf8');
 		sysdig.stdout.on('data', (data) => {
@@ -39,14 +52,20 @@ exports.load = (args, opts, cb) => {
 				let fdParser = /fd=(\d+)\(<([^>]+)>(?:([^) ]*):(.*)->([^) ]*):(.*)|(\/(?:[^)]+)))?\)/;
 				let parsedRawData = fdModel(fdParser.exec(parsedData.eventRawData));
 				if(parsedRawData) {
-					delete parsedData.eventRawData;
 					parsedData.data = parsedRawData;
 					jsome(parsedData);
 				}
 			}
-			if(opts.proc && (parsedData.eventType==='execve' || parsedData.eventType==='clone')) {
-				jsome(parsedData);
+			if(opts.proc && (parsedData.eventType==='execve' || parsedData.eventType==='clone')) { 
+				let procParser = /tid=(\d*)\(([^)]*)\) pid=(\d*)\(([^)]*)\) ptid=(\d*)\(([^)]*)\)/;
+				let parsedRawData = procModel(procParser.exec(parsedData.eventRawData));
+				if (parsedRawData) {
+					parsedData.relation = parsedRawData;
+					jsome(parsedData);
+				}
 			}
+			delete parsedData.eventRawData;
+
 		}
 	}
 
@@ -69,10 +88,10 @@ exports.load = (args, opts, cb) => {
 				return {
 					fdId:parseInt(parsed[1]),
 					fdType:parsed[2],
-					leftIp:parsed[3],
-					leftP:parsed[4],
-					rightIp:parsed[5],
-					rightP:parsed[6]
+					sourceIp:parsed[3],
+					sourceP:parsed[4],
+					targetIp:parsed[5],
+					targetP:parsed[6]
 				}
 			}
 			else if(parsed[7] !== void 0) {
@@ -85,6 +104,19 @@ exports.load = (args, opts, cb) => {
 		}
 		else {
 			return void 0;
+		}
+	}
+
+	function procModel(parsed) {
+		if (parsed) {
+			return {
+				tid: parsed[1],
+				tprocName: parsed[2],
+				pid: parsed[3],
+				pProcName: parsed[4],
+				ptid: parsed[5],
+				ptProceName: parsed[6]
+			}
 		}
 	}
 };
