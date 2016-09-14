0739542841609c6429f0e60a9de3f4bf2d5dfc4e
Joshua Lunsford <josh@decodingideas.com>
2016-09-14 17:40:54 +0000

    added start of ui

---
+++ src/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'psp-miner',
+    template: '<h1>psp miner!</h1>'
+})
+export class AppComponent { }
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/main.ts',
+    output: {
+        path: './dist',
+        filename: 'app.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.ts$/, loader: 'ts' }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
+};
