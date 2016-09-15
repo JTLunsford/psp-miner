0b0909b12d6206edb2138d6f4552ad6ae0ea7251
Joshua Lunsford <josh@decodingideas.com>
2016-09-15 02:03:22 +0000

    stuff

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+import 'core-js';
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
 import { AppComponent } from './app/app.component';
-
 
 @NgModule({
     declarations: [ AppComponent ],
