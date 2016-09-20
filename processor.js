449d5c497170533c2f347120482387bab0c3d5f0
Joshua Lunsford <josh@decodingideas.com>
2016-09-20 16:00:09 +0000

    made db.json flatter for easier simulation

--- processor.js
+++ processor.js
@@ -13,78 +13,83 @@ let dbLoaded = false;
 let db;
 function upsertProcess(procname, parent) {
 	cli.debug('upserting');
+	let item;
 	if(!_.some(_.map(config.skip, 'proc'),(n)=>{return n===procname || procname.match(n) !== null;})) {
 		cli.debug(`proc ${procname} not skipped`);
-		if(db.processes[procname] === void 0) {
-			db.processes[procname] = {
-				procname: procname,
+		item = _.find(db,(i) => { return i.name === procname;});
+		if(!item) {
+			db.push({
+				name: procname,
+				type: 'p',
 				children: [],
-				connections: [],
-				resources: [],
 				events: 1
-			};					
+			});					
 		}
 		else {
-			db.processes[procname].events++;
+			item.events++;
 		}
 		if(parent !== void 0) {
-			upsertProcess(parent);
-			if(db.processes[parent]){
-				db.processes[parent].children.push(procname);
-				db.processes[parent].children = _.uniq(db.processes[parent].children);
+			let p = upsertProcess(parent);
+			if(p){
+				p.children.push(procname);
+				p.children = _.uniq(p.children);
 			}
 		}
 	}
+	return item;
 }
 
 function upsertConnection(procname, ip) {
 	if(!_.some(config.ipSkip,(n)=>{return ip == n || _.some(new cidr().list(n), (anIp) => { return anIp == ip; });})) {
 		cli.debug(`connection ${ip} not skipped`);
-		if(db.connections[ip] === void 0) {
-			db.connections[ip] = {
-				ip: ip,
-				procs: [procname],
+		let item = _.find(db,(i) => { return i.name === ip;});
+		if(!item) {
+			db.push({
+				name: ip,
+				type: 'c',
+				parents: [procname],
 				events: 1
-			};					
+			});					
 		}
 		else {
-			db.connections[ip].events++;
-			db.connections[ip].procs.push(procname);
-			db.connections[ip].procs = _.uniq(db.connections[ip].procs);
+			item.events++;
+			item.parents.push(procname);
+			item.parents = _.uniq(item.procs);
 		}
-		db.processes[procname].connections.push(ip);
-		db.processes[procname].connections = _.uniq(db.processes[procname].connections);
+		let proc = _.find(db,(i) => { return i.name === procname;});
+		proc.children.push(ip);
+		proc.children = _.uniq(proc.children);
 	}
 }
 
 function upsertResource(procname, path) {
 	if(!_.some(config.pathSkip,(n)=>{return minimatch(n, path);})) {
 		cli.debug(`resource ${path} not skipped`);
-		if(db.resources[path] === void 0) {
-			db.resources[path] = {
-				path: path,
-				procs: [procname],
+		let item = _.find(db,(i) => { return i.name === path;});
+		if(!item) {
+			db.push( {
+				name: path,
+				type: 'r',
+				parents: [procname],
 				events: 1
-			};					
+			});					
 		}
 		else {
-			db.resources[path].events++;
-			db.resources[path].procs.push(procname);
-			db.resources[path].procs = _.uniq(db.resources[path].procs);
+			item.events++;
+			item.parents.push(procname);
+			item.parents = _.uniq(item.parents);
 		}
-		if(db.processes[procname]){
-			db.processes[procname].resources.push(path);
-			db.processes[procname].resources = _.uniq(db.processes[procname].resources);
+		
+		let proc = _.find(db,(i) => { return i.name === procname;});
+		if(proc){
+			proc.children.push(path);
+			proc.children = _.uniq(proc.children);
 		}
 	}
 }
 
 function initializeDb(cb) {
-	db = {
-		"connections":{},
-		"resources":{},
-		"processes":{}
-	};
+	db = [];
 
 	dbLoaded = true;
 
